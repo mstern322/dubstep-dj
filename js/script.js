2b986c09e80a1e90966fb2a40bad06b00242c163
mstern322 <mjs476@cornell.edu>
2012-07-04 13:02:46 -0700

    Added Flash 9 as default for multiShot capabilities and loaded sounds

--- js/script.js
+++ js/script.js
@@ -24,19 +24,30 @@ $(document).ready(function() {
 });
 
 soundManager.setup({
-  url: 'swf/',
+  url: 'http://www.humoruscomedy.com/sounds/',
+  debugMode: false,
+  useHighPerformance: true,
+  preferFlash: true,
+  flashVersion: 9,
   onready: function() {
     soundManager.createSound({
       id: 'clapSound',
-      url: 'clap.mp3'
+      url: 'http://www.humoruscomedy.com/sounds/clap.mp3'
     });
+    soundManager.load('clapSound');
     
     soundManager.createSound({
       id: 'kickSound',
       url: 'http://www.canadianmusicartists.com/sample/kick_05.wav'
     });
+    soundManager.load('kickSound');
+    soundManager.defaultOptions = {
+      multiShot: true,
+      multiShotEvents: true
+    };
   },
   ontimeout: function() {
     console.log('Error loading sound manager');
   }
+
 });
